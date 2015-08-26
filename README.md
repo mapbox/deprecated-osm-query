e19cf2ba399a148b4e23918778c9046299658547
Sajjad Anwar <me@sajjad.in>
2015-08-26 17:01:00 +0530

    browserify and instructions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # osm-query
 Planets, stars, nodes and ways
+
+# development
+* `npm install`
+* `browserify js/app.js > dist/bundle.js`
+* `serve`
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var overpass = require('query-overpass');
 L.mapbox.accessToken = 'pk.eyJ1IjoiZ2VvaGFja2VyIiwiYSI6ImFIN0hENW8ifQ.GGpH9gLyEg0PZf3NPQ7Vrg';
 // Create a map in the div #map
 var map = L.mapbox.map('map', 'mapbox.streets');
